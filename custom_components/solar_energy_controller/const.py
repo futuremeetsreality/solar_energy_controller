dcd3276dc2b9cb361fa1f06f6af69bff833f5298
futuremeetsreality <80096813+futuremeetsreality@users.noreply.github.com>
2026-08-10 20:32:12 +0200

    Add integration constants and defaults

---
+++ custom_components/solar_energy_controller/const.py
@@ -0,0 +1,69 @@
+# File: custom_components/solar_energy_controller/const.py
+# Timestamp: 2026-08-10 20:29 CEST
+
+from __future__ import annotations
+
+DOMAIN = "solar_energy_controller"
+PLATFORMS = ["sensor", "switch"]
+
+CONF_PV_POWER = "pv_power_entity"
+CONF_PV_ENERGY_TODAY = "pv_energy_today_entity"
+CONF_HOUSE_LOAD = "house_load_entity"
+CONF_GRID_EXPORT_POWER = "grid_export_power_entity"
+CONF_GRID_IMPORT_POWER = "grid_import_power_entity"
+CONF_GRID_EXPORT_ENERGY_TODAY = "grid_export_energy_today_entity"
+CONF_GRID_IMPORT_ENERGY_TODAY = "grid_import_energy_today_entity"
+CONF_BATTERY_SOC = "battery_soc_entity"
+CONF_BATTERY_POWER = "battery_power_entity"
+CONF_EPEX_PRICE = "epex_price_entity"
+CONF_EFFECTIVE_EXPORT_PRICE = "effective_export_price_entity"
+CONF_IMPORT_TOTAL_PRICE = "import_total_price_entity"
+CONF_MINER_SWITCH = "miner_switch_entity"
+CONF_MINER_POWER = "miner_power_entity"
+
+CONF_BATTERY_CAPACITY_KWH = "battery_capacity_kwh"
+CONF_BATTERY_MIN_SOC = "battery_min_soc"
+CONF_BATTERY_MAX_CHARGE_KW = "battery_max_charge_kw"
+CONF_BATTERY_MAX_DISCHARGE_KW = "battery_max_discharge_kw"
+CONF_BATTERY_CHARGE_EFFICIENCY = "battery_charge_efficiency"
+CONF_BATTERY_DISCHARGE_EFFICIENCY = "battery_discharge_efficiency"
+CONF_MINER_NOMINAL_POWER_KW = "miner_nominal_power_kw"
+CONF_MINER_MIN_RUNTIME_MIN = "miner_min_runtime_min"
+CONF_DOGE_PER_DAY = "doge_per_day"
+CONF_DOGE_TARGET_EUR = "doge_target_eur"
+CONF_IMPORT_FALLBACK_EUR_KWH = "import_fallback_eur_kwh"
+CONF_INTERVAL_MIN = "interval_min"
+CONF_HORIZON_H = "horizon_h"
+
+DEFAULTS = {
+    CONF_PV_POWER: "sensor.solax_inverter_pv_power_total",
+    CONF_PV_ENERGY_TODAY: "sensor.solax_inverter_today_s_solar_energy",
+    CONF_HOUSE_LOAD: "sensor.solax_inverter_house_load",
+    CONF_GRID_EXPORT_POWER: "sensor.solax_inverter_grid_export",
+    CONF_GRID_IMPORT_POWER: "sensor.solax_inverter_grid_import",
+    CONF_GRID_EXPORT_ENERGY_TODAY: "sensor.solax_inverter_today_s_export_energy",
+    CONF_GRID_IMPORT_ENERGY_TODAY: "sensor.solax_inverter_today_s_import_energy",
+    CONF_BATTERY_SOC: "sensor.solax_inverter_battery_1_capacity",
+    CONF_BATTERY_POWER: "sensor.solax_inverter_battery_1_power_charge",
+    CONF_EPEX_PRICE: "sensor.epex_spot_data_market_price",
+    CONF_EFFECTIVE_EXPORT_PRICE: "",
+    CONF_IMPORT_TOTAL_PRICE: "",
+    CONF_MINER_SWITCH: "switch.steckdose_mining",
+    CONF_MINER_POWER: "sensor.steckdose_mining_power",
+    CONF_BATTERY_CAPACITY_KWH: 12.5,
+    CONF_BATTERY_MIN_SOC: 25.0,
+    CONF_BATTERY_MAX_CHARGE_KW: 8.0,
+    CONF_BATTERY_MAX_DISCHARGE_KW: 8.0,
+    CONF_BATTERY_CHARGE_EFFICIENCY: 0.95,
+    CONF_BATTERY_DISCHARGE_EFFICIENCY: 0.95,
+    CONF_MINER_NOMINAL_POWER_KW: 1.0,
+    CONF_MINER_MIN_RUNTIME_MIN: 60,
+    CONF_DOGE_PER_DAY: 12.0,
+    CONF_DOGE_TARGET_EUR: 0.20,
+    CONF_IMPORT_FALLBACK_EUR_KWH: 0.348,
+    CONF_INTERVAL_MIN: 15,
+    CONF_HORIZON_H: 24,
+}
+
+STORAGE_VERSION = 1
+STORAGE_KEY = f"{DOMAIN}_state"
